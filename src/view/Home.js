1d8bae44701bd713afaacf2395a3197a856de196
Keven <kevenbarauna@gmail.com>
2020-07-30 16:34:00 -0300

    Add página home

---
+++ src/view/Home.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import Topo from './../components/Topo/Topo'
+
+class Home extends React.Component {
+    componentDidMount() { }
+
+    render() {
+        return (
+            <Topo />
+        );
+    }
+}
+
+const mapStateToPros = (state) => ({
+
+});
+
+export default Home;
