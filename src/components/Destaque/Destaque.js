b5a15583bb1557d0b260982c121d8e400489ab30
Keven <kevenbarauna@gmail.com>
2020-07-30 18:14:20 -0300

    Add padrões diferentes no destaque

--- src/components/Destaque/Destaque.js
+++ src/components/Destaque/Destaque.js
@@ -4,23 +4,47 @@ import './Destaque.css';
 
 const Destaque = (props) => {
     const {
-        titulo,
-        subTitulo,
-        imagem,
-        altImagem,
+        titulo, //TITULO DO TEXTO  [STRING]
+        subTitulo, //SUBTITULO DO TEXTO [STRING]
+        imagem, //IMAGEM [IMAGEM]
+        altImagem, //ALT QUE APARECE NA IMAGEM [STRING]
+        padrao, //TRUE = IMAGEM ESQUERDA | FALSE = IMAGEM DIREITA [BOOL]
     } = props;
+
+    function getClassName(nomeClasse) {
+        if (nomeClasse === 'destaque-titulo') {
+            if (props.padrao === false) { return 'destaque-titulo-false' } else { return 'destaque-titulo-true'; }
+        }
+        else if (nomeClasse === 'destaque-sub-titulo') {
+            if (props.padrao === false) { return 'destaque-sub-titulo-false' } else { return 'destaque-sub-titulo-true'; }
+        }
+        return 'destaque-titulo-true';
+    }
 
     return (
         <div className='destaque-background'>
             <Row className='destaque-container'>
                 <Col>
-                    <p className='destaque-titulo'>{props.titulo}</p>
-                    <p className='destaque-sub-titulo'>{props.subTitulo}</p>
+                    {padrao === true ? (
+                        <>
+                            <p className={getClassName('destaque-titulo')}>{props.titulo}</p>
+                            <p className={getClassName('destaque-sub-titulo')}>{props.subTitulo}</p>
+                        </>
+                    ) : (
+                            <img style={{ height: '300px', marginLeft: '41%' }} src={props.imagem} alt={props.altImagem}></img>
+                        )}
                 </Col >
             </Row>
             <Row className='destaque-container'>
                 <Col >
-                    <img style={{ height: '300px' }} src={props.imagem} alt={props.altImagem}></img>
+                    {padrao === true ? (
+                        <img style={{ height: '300px' }} src={props.imagem} alt={props.altImagem}></img>
+                    ) : (
+                            <>
+                                <p className={getClassName('destaque-titulo')}>{props.titulo}</p>
+                                <p className={getClassName('destaque-sub-titulo')}>{props.subTitulo}</p>
+                            </>
+                        )}
                 </Col>
             </Row>
         </div>
--- src/view/Home.js
+++ src/view/Home.js
@@ -3,6 +3,8 @@ import Topo from './../components/Topo/Topo';
 import Destaque from './../components/Destaque/Destaque';
 
 import imagemTv from './../assets/img/tv.png';
+import imagemCelular from './../assets/img/mobile.jpg';
+import imagemCelularTv from './../assets/img/tvMobile.png';
 
 class Home extends React.Component {
     componentDidMount() { }
@@ -17,6 +19,21 @@ class Home extends React.Component {
                     subTitulo={'Assista em Smart TVs, PlayStation, Xbox, Chromecast, Apple TV, aparelhos de Blu-ray e outros aparelhos.'}
                     imagem={imagemTv}
                     altImagem={'imagem tv'}
+                    padrao={true}
+                />
+                <Destaque
+                    titulo={'Baixe séries para assistir offline.'}
+                    subTitulo={'Salve seus títulos favoritos e sempre tenha algo para assistir.'}
+                    imagem={imagemCelular}
+                    altImagem={'imagem mobile'}
+                    padrao={false}
+                />
+                <Destaque
+                    titulo={'Assista quando quiser.'}
+                    subTitulo={'Assista no celular, tablet, smart TV ou notebook sem pagar a mais por isso.'}
+                    imagem={imagemCelularTv}
+                    altImagem={'imagem mobile e tv'}
+                    padrao={true}
                 />
             </>
         );
