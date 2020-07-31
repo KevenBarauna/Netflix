2d327fea137400893f4c1898335ed30ec2434c67
Keven <kevenbarauna@gmail.com>
2020-07-31 16:17:06 -0300

    Ajuste topo com react boorstrap

--- src/components/Topo/Topo.js
+++ src/components/Topo/Topo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Topo.css';
 import imagemLogo from './../../assets/img/logo.png';
@@ -8,18 +8,20 @@ import imagemLogo from './../../assets/img/logo.png';
 function Topo() {
     return (
         <header className='topo-header'>
-            <Row>
-                <div className='topo-nav'>
-                    <div className='topo-div-imagem'>
-                        <img className='topo-imagem-logo' src={imagemLogo} alt='logo KevenFlix'></img>
-                    </div>
-                    <div className='topo-menu-item'>
+            <Row style={{ padding: '10px' }}>
+                <Col className='topo-div-imagem'>
+                    <Link to='/home'>
+                        <img className='topo-imagem-logo' src={imagemLogo} alt='logo KevenFlix' ></img>
+                    </Link>
+                </Col>
+                <Col className='topo-nav'>
+                    <div>
                         <li><Link to='/404'>Alura</Link></li>
                         <li><Link to='/404'>Udemy</Link></li>
                         <li><Link to='/404'>Youtube</Link></li>
                         <li><Link to='/404'>Outros</Link></li>
                     </div>
-                </div>
+                </Col>
             </Row>
         </header>
     );
