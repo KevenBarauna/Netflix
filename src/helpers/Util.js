6889b4095f7ae35dd284ca7efe7a62d02918689d
Keven <kevenbarauna@gmail.com>
2020-07-31 16:02:26 -0300

    Add util

---
+++ src/helpers/Util.js
@@ -0,0 +1,15 @@
+export default class Util {
+
+    static get NOME_PROJETO() {
+        return 'KevenFlix';
+    }
+
+    static get COR() {
+        var cores = {};
+        cores.preto = '#141414';
+        cores.branco = '#ffffff';
+        cores.vermelho = '#db202c';
+        cores.cinza = '#757575;';
+        return cores;
+    }
+}
